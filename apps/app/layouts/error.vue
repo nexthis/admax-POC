829c687b4fd233ee7d589fc1aa5ca92075b57fd6
nexthis <pawel.romanowski000@gmail.com>
2023-06-05 13:34:42 +0200

    fix deploy

--- apps/app/layouts/error.vue
+++ apps/app/layouts/error.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Error404 from '@/components/shared/lp-banner/Banner404.vue';
+import { ref } from "vue";
+// import Error404 from '@/components/shared/lp-banner/Banner404.vue';
 </script>
 
 <template>
   <v-main>
-    <Error404/>
+    <!-- <Error404/> -->
+    404
   </v-main>
 </template>
-
-
